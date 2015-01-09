5545375a2b0f977c3366e50300d03fbbbf122a2a
Michael Steil <mist64@mac.com>
2015-01-09 16:19:29 +0100

    ...

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 ca65 fc3.s &&
+ca65 speeder.s && 
 ca65 monitor.s && 
 ca65 persistent.s && 
-ld65 -C fc3.cfg fc3.o monitor.o persistent.o -o fc3.bin &&
+ld65 -C fc3.cfg fc3.o speeder.o monitor.o persistent.o -o fc3.bin &&
 (
 hexdump -C fc3-orig.bin > /tmp/fc3-orig.bin.txt; hexdump -C fc3.bin > /tmp/fc3.bin.txt; diff -u /tmp/fc3-orig.bin.txt /tmp/fc3.bin.txt
 )
