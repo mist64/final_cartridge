94ccb2e113ecb1a8930f0fa230a409de0925773a
Michael Steil <mist64@mac.com>
2015-01-10 04:22:02 +0100

    ...

--- build.sh
+++ build.sh
@@ -5,9 +5,10 @@ ca65 speeder.s &&
 ca65 monitor.s && 
 ca65 wrappers.s && 
 ca65 printer.s && 
+ca65 format.s && 
 ca65 freezer.s && 
 ca65 persistent.s && 
-ld65 -C fc3.cfg fc3.o drive.o desktop_helper.o speeder.o monitor.o wrappers.o printer.o freezer.o persistent.o -o fc3.bin &&
+ld65 -C fc3.cfg fc3.o drive.o desktop_helper.o speeder.o monitor.o wrappers.o printer.o format.o freezer.o persistent.o -o fc3.bin &&
 (
 hexdump -C fc3-orig.bin > /tmp/fc3-orig.bin.txt; hexdump -C fc3.bin > /tmp/fc3.bin.txt; diff -u /tmp/fc3-orig.bin.txt /tmp/fc3.bin.txt
 )
