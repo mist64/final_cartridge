079168d3b45abf86d5288ae75e0de62ffd70643a
Daniel Mantione <daniel.mantione@freepascal.org>
2024-10-13 23:39:12 +0200

    * First step into decyphering the mysterycode.

--- bank0/desktop_helper.s
+++ bank0/desktop_helper.s
@@ -252,9 +252,9 @@ L968C:  ldy     $AE
         rts
 
 disk_operation_fallback:
-        lda     #<($FF92 - 1)
+        lda     #>($9200 - 1)
         pha
-        lda     #>($FF92 - 1) ; ???
+        lda     #<($9200 - 1)
         pha
         lda     #$43
         jmp     _jmp_bank ; bank 3
